f4a857cf48bc250f8e863a8a2d301040e3ed039a
Haikal79 <haikalanugerah79@gmail.com>
2025-06-03 14:43:13 +0700

    app.jsx

---
+++ frontend/app.jsx
@@ -0,0 +1,16 @@
+// KantinGo - Frontend React Component
+// Copyright (c) 2025 John Doe
+// Licensed under the MIT License
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Selamat Datang di KantinGo</h1>
+      <p>Pesan makanan favoritmu dengan mudah!</p>
+    </div>
+  );
+};
+
+export default App;
